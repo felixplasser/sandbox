403b40585172b838b8e01f2ab299324f78cf7c1c
cmfp2 <cmfp2@LNX-W243-1.LUNET.LBORO.AC.UK>
2024-08-12 14:50:31 +0100

    felix code

--- joint_prog.py
+++ joint_prog.py
@@ -10,5 +10,13 @@ def dong(n):
     pass
 
 def felix(n):
+    print("This is Felix' code!!")
     for i in range(n):
         print(i)
+
+if __name__=='__main__':
+    n = 3
+    james(n)
+    umatur(n)
+    dong(n)
+    felix(n)
